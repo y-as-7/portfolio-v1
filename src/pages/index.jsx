bca304927125803db2e4db5cf382c3b2e6cc4708
youssef askar <youssefaskar111@outlook.com>
2023-04-27 20:51:38 +0200

    create animated text comp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,10 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
-import profilePic from '../../public/images/profile/developer-pic-1.png'
+import profilePic from "../../public/images/profile/developer-pic-1.png";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icon";
 export default function Home() {
   return (
     <>
@@ -13,13 +16,35 @@ export default function Home() {
         <Layout className="pt-0 ">
           <div className="flex items-center justify-between w-full">
             <div className="w-1/2">
-              <Image src={profilePic} alt='img1' className='w-full h-auto'></Image>
+              <Image
+                src={profilePic}
+                alt="img1"
+                className="w-full h-auto"
+              ></Image>
             </div>
-            <div className="w-1/2 ">
-              <h1>Turning Vision Into Reality With Code And Design.</h1>
-              <p>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className=" !text-6xl !text-left "
+              />
+              <p className="my-4 text-base font-medium ">
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
+              <div className="flex items-center self-start mt-2 ">
+              <Link href="/dummy.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-2 border-solid border-transparent hover:border-dark"
+              download={true}>
+              Rescume
+              <LinkArrow className="w-6 pl-1"/>
+              </Link>
+              <Link  href="mailto:youssefaskar111@outlook.com" target={"_blank"} className="ml-4 text-lg font-medium capitalize text-dark underline ">
+                Contant
+              </Link>
             </div>
+            </div>
+            
           </div>
         </Layout>
       </main>
