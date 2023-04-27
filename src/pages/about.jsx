f70684d8c58540174798333648e98761e1cbcbf7
youssef askar <youssefaskar111@outlook.com>
2023-04-28 01:49:56 +0200

    skill comp

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import React, { useEffect, useRef } from "react";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -90,6 +91,7 @@ function about() {
               </div>
             </div>
           </div>
+          <Skills/>
         </Layout>
       </main>
     </>
